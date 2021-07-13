b4aa3bb0a6db02070c5fabaa5dcd2f4a21efff9e
Aryaman Srivastava <aryamansrivastava2006@gmail.com>
2021-07-13 07:55:59 +0530

    Delete djserver directory

--- djserver/api/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class TumorDetection(models.Model):
-    originalImg = models.ImageField(upload_to='oimgs')
-    processedImg = models.ImageField(upload_to='processedImg')
--- djserver/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-     path('imgrec', views.imgrec,name='imgrec'),
-]
